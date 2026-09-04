8eca2882578c234737c09fd7bb522bd4e94a0733
Arpit <ryadav28879@gmail.com>
2026-09-04 19:40:49 +0530

    multer middleware

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,19 @@
+import multer from "multer"
+
+const crypto = require('crypto')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/my-uploads')
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(16, function (err, raw) {
+      if (err) return cb(err)
+      cb(null, file.fieldname + '-' + raw.toString('hex'))
+    })
+  }
+})
+
+export const upload = multer({ 
+  storage: storage 
+})
